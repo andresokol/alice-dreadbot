c25b7fdb3cd8fe48807c68e060a070c90b477fc6
Andrey Sokolov <andresokol@yandex-team.ru>
2019-09-08 16:21:51 +0300

    test

--- main.py
+++ main.py
@@ -10,16 +10,30 @@ async def ping(request) -> web.Response:
     return web.json_response({'status': 'ok'})
 
 
-async def alice_twitch(request) -> web.Response:
-    return web.Response(text='ОК')
+async def alice_twitch_get(request) -> web.Response:
+    print('ok')
+    return web.json_response({})
+
+
+async def alice_twitch_post(request) -> web.Response:
+    body = await request.json()
+    response = {
+        'version': body['version'],
+        'session': body['session'],
+        'response': {
+            'end_session': False,
+            'text': 'Лол'
+        }
+    }
+    return web.json_response(response)
 
 
 def create_app() -> web.Application:
     app = web.Application()
 
     app.router.add_get('/', ping)
-    app.router.add_get('/alice-twitch/', alice_twitch)
-    app.router.add_post('/alice-twitch/', alice_twitch)
+    app.router.add_get('/alice-twitch/', alice_twitch_get)
+    app.router.add_post('/alice-twitch/', alice_twitch_post)
 
     return app
 
